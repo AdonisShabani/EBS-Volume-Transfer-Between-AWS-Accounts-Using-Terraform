44c71dc1b7312f80ca7fec145380b199357a11bf
AdonisShabani <adonisshabani12@gmail.com>
2023-08-06 14:00:51 +0200

    commented resources

--- terraform/networking/providers.tf
+++ terraform/networking/providers.tf
@@ -21,18 +21,18 @@ provider "aws" {
   }
 }
 
-provider "aws" {
-  aws_region            = var.aws_region
-  aws_access_key        = var.aws_access_key
-  aws_secret_access_key = var.aws_secret_access_key
-  aws_session_token     = var.aws_session_token
-  alias                 = "sandbox"
+# provider "aws" {
+#   aws_region            = var.region
+#   aws_access_key        = var.aws_access_key
+#   aws_secret_access_key = var.aws_secret_access_key
+#   aws_session_token     = var.aws_session_token
+#   alias                 = "training"
 
 
-  default_tags {
-    Environment = var.environment
-    Project     = local.project
-    ManagedBy   = "Terraform"
-    Region      = var.region
-  }
-}
+#   default_tags {
+#     Environment = var.environment
+#     Project     = local.project
+#     ManagedBy   = "Terraform"
+#     Region      = var.region
+#   }
+# }

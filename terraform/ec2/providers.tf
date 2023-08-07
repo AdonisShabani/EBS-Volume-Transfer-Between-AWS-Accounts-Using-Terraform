4b3abfd2507a55cf4895d90418b2d029281feefd
AdonisShabani <adonisshabani12@gmail.com>
2023-08-07 13:34:25 +0200

    minor fixes

--- terraform/ec2/providers.tf
+++ terraform/ec2/providers.tf
@@ -7,34 +7,36 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   region     = var.region
-#   access_key = var.aws_access_key
-#   secret_key = var.aws_secret_access_key
-#   token      = var.aws_session_token
-
-#   default_tags {
-#     tags = {
-#       Environment = var.environment
-#       Project     = local.project
-#       ManagedBy   = "Terraform"
-#       Region      = var.region
-#     }
-#   }
-# }
-
 provider "aws" {
-  aws_region            = var.region
-  aws_access_key        = var.aws_access_key
-  aws_secret_access_key = var.aws_secret_access_key
-  aws_session_token     = var.aws_session_token
-  alias                 = "training"
-
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+  token      = var.aws_session_token
 
   default_tags {
-    Environment = var.environment
-    Project     = local.project
-    ManagedBy   = "Terraform"
-    Region      = var.region
+    tags = {
+      Environment = var.environment
+      Project     = local.project
+      ManagedBy   = "Terraform"
+      Region      = var.region
+    }
   }
 }
+
+ provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+  token      = var.aws_session_token
+  alias      = "training"
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = local.project
+      ManagedBy   = "Terraform"
+      Region      = var.region
+    }
+  }
+}
+

7e726ab3c3283c858ee62a99fb357fa27a86adfb
AdonisShabani <adonisshabani12@gmail.com>
2023-08-10 15:14:48 +0200

    add EC2 and Security Group

--- terraform/ec2/data.tf
+++ terraform/ec2/data.tf
@@ -2,3 +2,13 @@ data "aws_instance" "ec2" {
   instance_id = "i-0739a8644a96b6110"
   provider    = aws.training
 }
+
+data "terraform_remote_state" "networking" {
+  backend = "remote"
+
+  config = {
+    workspaces   = { name = "networking-training-account" }
+    hostname     = "app.terraform.io"
+    organization = "adonis1995"
+  }
+}
--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -10,22 +10,58 @@ resource "aws_ami_launch_permission" "ami_premission" {
   provider = aws.training
 }
 
-# module "ec2_instance" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
+module "ec2_instance_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.2"
 
-#   name = "ec2-destination-instance"
+  name        = "my-sg"
+  description = "EC2 Security Group"
+  vpc_id      = data.terraform_remote_state.networking.outputs.vpc_id
 
-#   ami = aws_ami_from_instance.ami.id
-#   associate_public_ip_address = true
-#   instance_type          = "t3.micro"
-#   key_name               = "destination-key"
-#   monitoring             = true
-#   vpc_security_group_ids = ["sg-12345678"]
-#   subnet_id              = "subnet-eddcdzz4"
-#   private_ip = "10.0.20.1"
+  # ingress with source sg
+  ingress_with_cidr_blocks = [
+    {
+      description              = "Allow incoming HTTP traffic"
+      from_port                = 80
+      to_port                  = 80
+      protocol                 = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      description              = "Allow incoming SSH traffic"
+      from_port                = 22
+      to_port                  = 22
+      protocol                 = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
 
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#   }
-# }
+  ]
+  
+  # egress
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+}
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "ec2-destination-instance"
+
+  ami = aws_ami_from_instance.ami.id
+  associate_public_ip_address = true
+  instance_type          = "t3.micro"
+  key_name               = "destination-key"
+  vpc_security_group_ids = [module.ec2_instance_sg.security_group_id]
+  subnet_id              = element(data.terraform_remote_state.networking.outputs.public_subnets, 1)
+  private_ip = "10.0.20.1"
+
+  tags = {
+    Environment = "dev"
+  }
+}
